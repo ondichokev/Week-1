1e7612d1cb0bf51a38badc8452bc7ecfe130fbc5
ondichokev <ondichokev@gmail.com>
2024-07-04 07:21:57 +0300

    Add files via upload

---
+++ grades.js
@@ -0,0 +1,31 @@
+function calculateGrade(mark) {
+    if (mark> 79) {
+        return 'A';
+    }else if (mark>=60){
+        return 'B';
+    }else if (mark>=50) {
+        return 'C';
+    }else if (mark >=40) {
+        return 'D';
+    }else {
+        return 'E';
+    }
+    }
+
+    function studentGradeGenerator() {
+        const mark =parseFloat(prompt('Enter the student mark (0-100):'));
+
+        if (isNaN(mark)|| mark < 0 || mark >100) {
+            return 'Invalid input.';
+        }
+
+        const grade = calculateGrade(mark);
+        return 'The student grade is: ${grade}';
+    }
+
+
+    const result = studentGradeGenerator();
+    console.log(result);
+
+
+    
